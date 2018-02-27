82a8e347fe379ecc3e2ed46c5d33450659b1ec9f
Yash Agrawal <yagrawal900@gmail.com>
2018-02-28 04:30:37 +0530

    Ported to Gtk3, updated sugargame and metadata

--- activity.py
+++ activity.py
@@ -3,7 +3,8 @@
 
 import sugargame
 import sugargame.canvas
-from sugar.activity import activity
+import pygame
+from sugar3.activity import activity
 
 import conozco
 
@@ -13,9 +14,10 @@ class Activity(activity.Activity):
         activity.Activity.__init__(self, handle)
 
         self.max_participants = 1
-        self.actividad = conozco.Conozco(self)
-        self._pygamecanvas = sugargame.canvas.PygameCanvas(self)
-        self.set_canvas(self._pygamecanvas)
-        self._pygamecanvas.grab_focus()
-        self._pygamecanvas.run_pygame(self.actividad.principal)
+        self.game = conozco.Conozco(self)
+        self.game.canvas = sugargame.canvas.PygameCanvas(self,
+            main=self.game.principal,
+            modules=[pygame.display, pygame.font])
+        self.set_canvas(self.game.canvas)
+        self.game.canvas.grab_focus()
 
